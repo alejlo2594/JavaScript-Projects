cf678ca1d1424b9dcf7d9d9778a694bc20895ca4
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-10-10 23:03:22 -0400

    Added main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,4 +1,10 @@
 function My_First_Function() {
-    var str="This is the button text";
+    var str="This is the button text"; //This will be the button
     document.getElementById("Button_Text").innerHTML=str;
+}
+
+function myFunction() {
+    var sentence="I am learning"; // Will be written in text
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
 }
