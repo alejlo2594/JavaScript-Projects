cfb85e342b312ce1111bd671d2d8d21ada22e594
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-10-10 22:39:37 -0400

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,2 +1,5 @@
-var Family="The Arrezzinis", Dad="Jeremiah", Mom="Hermione", Daughter="Penny", Son="Zorro";
-document.write(Dad);
+var X="Welcome to the page!" //This will appear in a pop up window
+window.alert(X)
+document.write("\"Be who you are and say what you feel," //Text will be shown on the page
++"because those who mind don\'t matter and those who matter don\'t mind.\"")
++"-Dr. Seuss)";
