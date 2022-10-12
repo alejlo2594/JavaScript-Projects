ca9fc66d8e81fef79a0d899d157c2b05fb3d8643
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-10-12 13:22:28 -0400

    Update main.js

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,2 +1,16 @@
-document.write(typeof "Word");
-document.write("15" + 10);
+document.write(typeof "Word"); // Using document.write() method and typeof operator
+document.write("15" + 10); // Expression combining a string and a number
+
+document.write("<br>"); 
+document.write(10==10); // Using == Operator
+
+document.write("<br>"); 
+X=5;
+Y=7;
+document.write(X===Y) // Using === Operator
+
+document.write("<br>"); 
+document.write(5 > 2 && 10 > 4); // Using >, && Operators
+
+document.write("<br>"); 
+document.write(10 < 5 || 10 < 20); // Using <, || Operators
