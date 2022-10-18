f0b3d87f643dd42414ba545bfb49044c51b21963
Alejandra Lopez <113959613+alejlo2594@users.noreply.github.com>
2022-10-17 20:16:03 -0400

    Updated main.js

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,4 @@
-function count_To_Ten() {
+function count_To_Ten() { //While loop
     var Digit = "";
     var X = 1;
     while (X < 11) {
@@ -7,3 +7,33 @@ function count_To_Ten() {
     }
     document.getElementById("Counting_to_Ten").innerHTML = Digit;
 }
+
+document.write("<br>") // For Loop
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+    Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+document.write("<br>") // Array
+function cat_pics() {
+    var Cat_Picture = [];
+    Cat_Picture[0] = "sleeping";
+    Cat_Picture[1] = "playing";
+    Cat_Picture[2] = "eating";
+    Cat_Picture[3] = "purring";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " + Cat_Picture[3] + ".";
+}
+
+document.write("<br>") // Creating an object with 'Let' keyword
+var A = 82;
+document.write(A);
+{
+    let A = 33;
+    document.write("<br>" + A);
+}
+document.write("<br>" + A);
